813046432df051e6d6b08ba783f815c7c16ff470
Ludovic <ludovic@maxken.fr>
2017-08-10 18:21:29 +0200

    Finish auto

---
+++ index.ios.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+import SearchPage from './SearchPage';
+
+class PropertyFinder extends Component {
+  render(){
+    return(
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Chercheur de propriété',
+          component: SearchPage,
+        }} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+  container: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('TutoReactLudo', () => PropertyFinder);
